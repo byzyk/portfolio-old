c36f19ce9de387de913db191c3125cb76cbb85b0
Bohdan Kh <bohdan.kh@gmail.com>
2013-01-04 19:11:37 +0200

    Main page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -14,8 +13,10 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.set('view engine', 'jade');  
+  app.locals.pretty = true;
+  app.set('title', 'Bohdan Khodakovsky');
+  app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -27,9 +28,7 @@ app.configure(function(){
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
-
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
-
-/*
- * GET home page.
- */
+var fs = require('fs');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var contacts = [
+    ['email', 'bohdan.kh@gmail.com'],
+    ['skype', 'byzyk93'],
+    ['phone', '+380 93 661 4427']
+  ];
+  
+  var json;
+  var works = [];
+  var worksDir = './works/';
+  var worksFiles = fs.readdirSync(worksDir);
+  worksFiles.sort(function(a, b) {
+    return fs.statSync(worksDir + a).mtime.getTime() - fs.statSync(worksDir + b).mtime.getTime();
+  });
+  worksFiles.reverse();
+  for (var i = 0; i < worksFiles.length; i++) {
+    json = fs.readFileSync(worksDir + worksFiles[i], 'utf-8');
+    works.push(JSON.parse(json));
+  }
+  console.log(works)
+  
+  res.render('index', {
+    contacts: contacts,
+    works: works
+  });
 };
