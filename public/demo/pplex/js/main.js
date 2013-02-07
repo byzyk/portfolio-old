59d086bf35ed15b3500280c81362499e9dc46ca8
Bohdan Kh <bohdan.kh@gmail.com>
2013-02-07 17:20:19 +0200

    Pplex Demo

---
+++ public/demo/pplex/js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('#login').popover({
+    html: true,
+    placement: 'bottom',
+    title: 'You can sign in with your <a href="http://pplex.net/users/auth/facebook">Facebook</a> or <a href="http://pplex.net/users/auth/google_oauth2">Google</a> accounts <br><br> <input type="text" style="width:195px;"> <input style="width:195px;" type="password"> <button class="btn btn-inverse" type="button" style="margin-bottom:-10px;">Sign in</button>'});
+});
