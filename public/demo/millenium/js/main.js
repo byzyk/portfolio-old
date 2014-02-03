c43179f7084bc32a70087b6c415419cd5d6a9457
Bohdan Kh <bohdan.kh@gmail.com>
2014-02-03 02:48:09 +0200

    MV final

--- public/demo/millenium/js/main.js
+++ public/demo/millenium/js/main.js
@@ -1,1 +1,15 @@
+$(function() {
 
+    $('input[type="checkbox"]').iCheck();
+
+    $('select').select2();
+
+
+    $(".order-link").click(function(e){
+        e.preventDefault();
+        $("#order-modal").modal({
+            opacity: 100
+        });
+    });
+
+});
